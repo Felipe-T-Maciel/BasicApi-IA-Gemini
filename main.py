6df00f76f51bfdc083fc9cef32f891defb1a876b
Felipe Tomio Maciel <felipetomio@weg.net>
2024-04-01 15:23:26 -0300

    save

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
 
-API_KEY = 'AIzaSyDXdK7r1MIldNMwshcRYIXStM8QnMA3HPs'
+API_KEY = 'AIzaSyAgNztT-4SvHysro8CP11XztH7fCCMwJtQ'
 ia.configure(api_key=API_KEY)
 model = ia.GenerativeModel('gemini-pro')
 
@@ -11,6 +11,6 @@ class Body(BaseModel):
     text: str
 
 @app.post("/ia")
-def getIA(body:Body):
-    response = model.generate_content((body.text, " não formate o texto e resuma o texto"))
+def post(body:Body):
+    response = model.generate_content(("Essas comidas estao disponiveis no bife de hoje, diga apenas UMA combinação e quantidade de cada pegar para estar mais saudavel e fale sobre em que pode beneficiar na minha saude. comida = ", body.text, ". não formate o texto e resuma o texto"))
     return response.text
